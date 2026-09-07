5901f2edb7aa25f9dd7f82d819869be9e6ad8543
Amjad Adi <1230800@student.birzeit.edu>
2026-09-07 10:50:08 +0300

    solve conflict to merge

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from 'react-router';
 import { Login } from '../features/auth/pages/login/Login.tsx';
 import { AuthLayout } from '../features/auth/layouts/Auth.tsx';
 import { Register } from '../features/auth/pages/register/Register.tsx';
-import { Users } from 'lucide-react';
 import { ManagementLayout } from '../features/management/layout/main-management.tsx';
 import { User } from '../features/management/admin-view/pages/UserPage.tsx';
 
